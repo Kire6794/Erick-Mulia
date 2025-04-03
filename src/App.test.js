b484d584146dba82d600e4b90834516c5c51231b
Kire6794 <erick671994@gmail.com>
2025-04-03 13:02:30 -0600

    update app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Erick Alexander Mulia Goycoolea/i);
+  expect(nameElement).toBeInTheDocument();
 });
